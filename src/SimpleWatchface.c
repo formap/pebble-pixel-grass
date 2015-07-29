e51ccb6e764b94e8a0bf4cbd54baa7885bd584d5
Florencia Tarditti <maria.florencia.tarditti@est.fib.upc.edu>
2015-07-29 19:34:18 +0200

    pebble watchface tutorial part 2.1: adding a custom font

--- src/SimpleWatchface.c
+++ src/SimpleWatchface.c
@@ -2,6 +2,7 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
 
 static void update_time() {
   time_t temp = time(NULL);
@@ -21,11 +22,12 @@ static void update_time() {
 static void main_window_load(Window *window) {
 
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
 
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
@@ -35,6 +37,7 @@ static void main_window_load(Window *window) {
 
 static void main_window_unload(Window *window) {
 
+  fonts_unload_custom_font(s_time_font);
   text_layer_destroy(s_time_layer);
 }
 
