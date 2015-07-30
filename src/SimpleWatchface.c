ca9971139d2340151f21e1aab9dd1b60402621b1
Florencia Tarditti <maria.florencia.tarditti@est.fib.upc.edu>
2015-07-30 21:56:27 +0200

    pebble watchface tutorial part 2.2: adding a bitmap

--- src/SimpleWatchface.c
+++ src/SimpleWatchface.c
@@ -3,6 +3,8 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static GFont s_time_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
 
 static void update_time() {
   time_t temp = time(NULL);
@@ -21,10 +23,15 @@ static void update_time() {
 
 static void main_window_load(Window *window) {
 
-  s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+
+  s_time_layer = text_layer_create(GRect(5, 30, 139, 48));
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
   text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00");
 
   text_layer_set_font(s_time_layer, s_time_font);
@@ -37,6 +44,8 @@ static void main_window_load(Window *window) {
 
 static void main_window_unload(Window *window) {
 
+  gbitmap_destroy(s_background_bitmap);
+  bitmap_layer_destroy(s_background_layer);
   fonts_unload_custom_font(s_time_font);
   text_layer_destroy(s_time_layer);
 }
