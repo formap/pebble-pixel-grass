426fc608bc975cd3034b9669b10b15e7f9d14ea5
Florencia Tarditti <maria.florencia.tarditti@est.fib.upc.edu>
2015-08-02 20:54:46 +0200

    pebble watchface tutorial part 3: adding weather

--- src/SimpleWatchface.c
+++ src/SimpleWatchface.c
@@ -1,8 +1,15 @@
 #include <pebble.h>
 
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
+
 static Window *s_main_window;
+
 static TextLayer *s_time_layer;
+static TextLayer *s_weather_layer;
+
 static GFont s_time_font;
+
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
@@ -29,15 +36,24 @@ static void main_window_load(Window *window) {
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
 
   s_time_layer = text_layer_create(GRect(5, 30, 139, 48));
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00");
 
-  text_layer_set_font(s_time_layer, s_time_font);
+  //s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+
+  s_weather_layer = text_layer_create(GRect(0, 85, 144, 85));
+  text_layer_set_background_color(s_weather_layer, GColorClear);
+  text_layer_set_text_color(s_weather_layer, GColorWhite);
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_weather_layer, "Loading...");
+
+  text_layer_set_font(s_weather_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
 
   update_time();
 }
@@ -46,13 +62,68 @@ static void main_window_unload(Window *window) {
 
   gbitmap_destroy(s_background_bitmap);
   bitmap_layer_destroy(s_background_layer);
+
   fonts_unload_custom_font(s_time_font);
   text_layer_destroy(s_time_layer);
+
+  text_layer_destroy(s_weather_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-
   update_time();
+
+  // Get weather update every 30 minutes
+  if(tick_time->tm_min % 30 == 0) {
+    // Begin dictionary
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+
+    // Add a key-value pair
+    dict_write_uint8(iter, 0, 0);
+
+    // Send the message!
+    app_message_outbox_send();
+  }
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  static char temperature_buffer[8];
+  static char conditions_buffer[32];
+  static char weather_layer_buffer[32];
+
+  Tuple *t = dict_read_first(iterator);
+
+  while(t != NULL) {
+    switch(t->key) {
+      case KEY_TEMPERATURE:
+        snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)t->value->int32);
+        break;
+      case KEY_CONDITIONS:
+        snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
+        break;
+      default:
+        APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized", (int)t->key);
+        break;
+    }
+
+    t = dict_read_next(iterator);
+  }
+
+  //snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+  snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s", temperature_buffer);
+  text_layer_set_text(s_weather_layer, weather_layer_buffer);
 }
 
 static void init() {
@@ -67,6 +138,13 @@ static void init() {
   window_stack_push(s_main_window, true);
 
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 }
 
 static void deinit() {
---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,63 @@
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function locationSuccess(pos) {
+  //Weather request
+  var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
+            pos.coords.latitude + '&lon=' + pos.coords.longitude;
+
+  xhrRequest(url, 'GET',
+    function(responseText) {
+      var json = JSON.parse(responseText);
+
+      var temperature = Math.round(json.main.temp - 273.15);
+      console.log('Temperature is ' + temperature);
+
+      var conditions = json.weather[0].main;
+      console.log('Conditions are ' + conditions);
+
+      var dictionary = {
+        'KEY_TEMPERATURE': temperature,
+        'KEY_CONDITIONS': conditions
+      };
+
+      Pebble.sendAppMessage(dictionary,
+        function(e) {
+          console.log('Weather info sent to Pebble');
+        },
+        function(e) {
+          console.log('Error sending the weather to the Pebble');
+        }
+      );
+    }
+  );
+}
+
+function locationError(err) {
+  console.log('Error requesting location');
+}
+
+function getWeather() {
+  navigator.geolocation.getCurrentPosition(
+    locationSuccess,
+    locationError,
+    {timeout: 15000, maximumAge: 60000}
+  );
+}
+
+Pebble.addEventListener('ready', function(e) {
+  console.log('PebbleKit JS is ready');
+
+  getWeather();
+});
+
+Pebble.addEventListener('appmessage', function(e) {
+  console.log('AppMessage received');
+  getWeather();
+});
