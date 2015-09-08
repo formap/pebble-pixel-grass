63b1571455f72153ae7d6743bdc5366d3fa741df
Florencia Tarditti <maria.florencia.tarditti@est.fib.upc.edu>
2015-09-08 17:59:11 +0200

    loads the user values when page is opened

--- configuration.js
+++ configuration.js
@@ -1,14 +1,18 @@
-function appendHours() {
+function appendHours () {
   var hour
-  for (var i = 0; i < 24; ++i) {
+  for (var i = 0; i < 60; ++i) {
     hour = i + ':00'
-    $('#startHour').append('<option value="' + hour + '">' + hour + '</option>')
-    $('#endHour').append('<option value="' + hour + '">' + hour + '</option>')
+    $('#startHour').append('<option value="' + i + '">' + hour + '</option>')
+    $('#endHour').append('<option value="' + i + '">' + hour + '</option>')
   }
 }
 
-function loadUserValues() {
-  // load the user values
+function urlParam (name) {
+  var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  if (!results) {
+    return 0;
+  }
+  return results[1] || 0;
 }
 
 $('#vibrationCheckbox').change(function() {
@@ -21,29 +25,49 @@ $('#vibrationCheckbox').change(function() {
   }
 }).trigger('change')
 
-$('#saveButton').click(function() {
-  var vibrations = $('#vibrationCheckbox').prop('checked')
-  var startHour
-  var endHour
-  if (vibrations) {
-    startHour = $('#startHour').val()
-    endHour = $('#endHour').val()
-  }
+function updateCheckbox (vibrations) {
+  $('#vibrationCheckbox').prop('checked', vibrations)
+}
 
-  var data = {
+function updateHours (startHour, endHour) {
+  $('#startHour [value="' + startHour + '"]').prop('selected', true)
+  $('#endHour [value=' + endHour + ']').prop('selected', true)
+}
+
+function loadUserValues () {
+  var vibrations = decodeURIComponent( urlParam("KEY_VIBRATIONS"))
+  var startHour = decodeURIComponent( urlParam("KEY_START_HOUR"))
+  var endHour = decodeURIComponent( urlParam("KEY_END_HOUR"))
+
+  updateCheckbox(vibrations)
+  $('#vibrationCheckbox').change()
+  updateHours(startHour, endHour)
+}
+
+var data
+
+$('#saveButton').click(function () {
+  $('.logs').append('save button pressed')
+
+  var vibrations = $('#vibrationCheckbox').prop('checked')
+  var startHour = $('#startHour').val()
+  var endHour = $('#endHour').val()
+
+  data = {
     'KEY_VIBRATIONS': vibrations,
     'KEY_START_HOUR': startHour,
     'KEY_END_HOUR': endHour
   }
+  $('.logs').append('data -> vibrations: ' + data.KEY_VIBRATIONS + ' startHour: ' + data.KEY_START_HOUR + ' endHour: ' + data.KEY_END_HOUR + ' ')
 
   document.location = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(data))
 })
 
-$('#cancelButton').click(function() {
+$('#cancelButton').click(function () {
   document.location = 'pebblejs://close'
 })
 
-$(document).ready(function() {
-  loadUserValues()
+$(document).ready(function () {
   appendHours()
+  loadUserValues()
 });
